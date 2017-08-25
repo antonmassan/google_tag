235b3ec5c18a2add514f9a15756f18e7a4e7fcd5
Ievgen_Antonovych <antevgen@gmail.com>
2017-08-25 11:03:36 +0300

    Updated all changes that not merged in original source: Issue #15 - Strict Mode Compliant YAML, Issue #9 - switch method types and made container and compact their own methods, Adapt to new format

--- src/EventSubscriber/GoogleTagResponseSubscriber.php
+++ src/EventSubscriber/GoogleTagResponseSubscriber.php
@@ -1,12 +1,9 @@
 <?php
-
 /**
  * @file
  * Contains GoogleTagResponseSubscriber.
  */
-
 namespace Drupal\google_tag\EventSubscriber;
-
 use Drupal\Component\Utility\Unicode;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Path\AliasManagerInterface;
@@ -19,48 +16,41 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
-
 /**
  * Class GoogleTagResponseSubscriber
  * @package Drupal\google_tag\EventSubscriber
  */
 class GoogleTagResponseSubscriber implements EventSubscriberInterface {
-
   /**
    * The config object for the google_tag settings.
    *
    * @var \Drupal\Core\Config\ImmutableConfig
    */
   protected $config;
-
   /**
    * An alias manager to find the alias for the current system path.
    *
    * @var \Drupal\Core\Path\AliasManagerInterface
    */
   protected $aliasManager;
-
   /**
    * The path matcher.
    *
    * @var \Drupal\Core\Path\PathMatcherInterface
    */
   protected $pathMatcher;
-
   /**
    * The current path.
    *
    * @var \Drupal\Core\Path\CurrentPathStack
    */
   protected $currentPath;
-
   /**
    * The current user.
    *
    * @var \Drupal\Core\Session\AccountProxyInterface
    */
   protected $currentUser;
-
   /**
    * Constructs a new Google Tag response subscriber.
    *
@@ -82,8 +72,6 @@ class GoogleTagResponseSubscriber implements EventSubscriberInterface {
     $this->currentPath = $current_path;
     $this->currentUser = $current_user;
   }
-
-
   /**
    * @param \Symfony\Component\HttpKernel\Event\FilterResponseEvent $event
    */
@@ -91,23 +79,70 @@ class GoogleTagResponseSubscriber implements EventSubscriberInterface {
     if (!$event->isMasterRequest()) {
       return;
     }
-
     $request = $event->getRequest();
     $response = $event->getResponse();
-
     if ($this->tagApplies($request, $response)) {
-      $container_id = $this->config->get('container_id');
-      $container_id = trim(json_encode($container_id), '"');
-      $compact = $this->config->get('compact_tag');
-
+      $container_id = $this->getContainerId();
+      $compact = $this->getCompact();
       // Insert snippet after the opening body tag.
-      $response_text = preg_replace('@<body[^>]*>@', '$0' . $this->getTag($container_id, $compact), $response->getContent(), 1);
+      $pattern = [
+        '@</head>@',
+        '@<body[^>]*>@'
+      ];
+      $replace = [
+        $this->getHeadTag($container_id, $compact) . '$0',
+        '$0' . $this->getTag($container_id, $compact)
+      ];
+      $response_text = preg_replace($pattern, $replace, $response->getContent(), 1);
       $response->setContent($response_text);
     }
   }
+  /**
+   * Fetch the container id, here to make it easier to overload.
+   *
+   * @return string
+   */
+  public function getContainerId() {
+    $container_id = $this->config->get('container_id');
+    return trim(json_encode($container_id), '"');
+  }
+
+  public function getCompact() {
+    return $this->config->get('compact_tag');
+  }
 
   /**
-   * Return the text for the tag.
+   * Return the text for the head tag.
+   *
+   * @param string $container_id
+   *   The Google Tag Manager container ID.
+   * @param bool $compact
+   *   Whether or not the tag should be compacted (whitespace removed).
+   *
+   * @return string
+   *   The full text of the Google Tag manager script/embed.
+   */
+  public function getHeadTag($container_id, $compact = FALSE) {
+    // Build script tags.
+    $script = <<<EOS
+<script type="text/javascript">
+(function(w,d,s,l,i){
+  w[l]=w[l]||[];
+  w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+  var f=d.getElementsByTagName(s)[0];
+  var j=d.createElement(s);
+  var dl=l!='dataLayer'?'&l='+l:'';
+  j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;
+  j.type='text/javascript';
+  j.async=true;
+  f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','$container_id');
+</script>
+EOS;
+    return $this->prepareTag($script, $compact);
+  }
+  /**
+   * Return the text for the body tag.
    *
    * @param string $container_id
    *   The Google Tag Manager container ID.
@@ -123,39 +158,39 @@ class GoogleTagResponseSubscriber implements EventSubscriberInterface {
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id=$container_id"
  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 EOS;
-    $script = <<<EOS
-<script type="text/javascript">
-(function(w,d,s,l,i){
+    return $this->prepareTag($noscript, $compact);
+  }
+  /**
+   * @param string $script
+   * @param bool $compact
+   * @return string
+   */
+  private function prepareTag($script, $compact) {
+    $script = ($compact) ? $this->compressHtml($script) : $script;
+    return $this->addComments($script);
+  }
+  /**
+   * Return the HTML compressed.
+   *
+   * @param string $data
+   * @return mixed
+   */
+  private function compressHtml($data) {
+    return str_replace(["\n", '  '], '', $data);
+  }
 
-  w[l]=w[l]||[];
-  w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-  var f=d.getElementsByTagName(s)[0];
-  var j=d.createElement(s);
-  var dl=l!='dataLayer'?'&l='+l:'';
-  j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;
-  j.type='text/javascript';
-  j.async=true;
-  f.parentNode.insertBefore(j,f);
-
-})(window,document,'script','dataLayer','$container_id');
-</script>
-EOS;
-
-
-    if ($compact) {
-      $noscript = str_replace("\n", '', $noscript);
-      $script = str_replace(array("\n", '  '), '', $script);
-    }
-    $script = <<<EOS
-
+  /**
+   * Return the comments around the script.
+   *
+   * @param string $script
+   * @return string
+   */
+  private function addComments($script) {
+    return <<<EOS
 <!-- Google Tag Manager -->
-$noscript
 $script
 <!-- End Google Tag Manager -->
 EOS;
-
-    return $script;
-
   }
 
   /**
@@ -169,32 +204,26 @@ EOS;
    * @return bool
    *   Whether or not the tag should be included on the page.
    */
-  private function tagApplies(Request $request, Response $response) {
+  protected function tagApplies(Request $request, Response $response) {
     $id = $this->config->get('container_id');
-
     if (empty($id)) {
       // No container ID.
       return FALSE;
     }
-
     if (!$this->statusCheck($response) && !$this->pathCheck($request)) {
       // Omit snippet based on the response status and path conditions.
       return FALSE;
     }
-
     if (!$this->roleCheck()) {
       // Omit snippet based on the response status and path conditions.
       return FALSE;
     }
-
     if (!($response instanceof HtmlResponse)) {
       // Omit snippet because the response is not HTML.
       return FALSE;
     }
-
     return TRUE;
   }
-
   /**
    * HTTP status code check. This checks to see if status check is even used
    * before checking the status.
@@ -206,13 +235,11 @@ EOS;
    *   True if the check is enabled and the status code matches the list of
    *   enabled statuses.
    */
-  private function statusCheck(Response $response) {
+  protected function statusCheck(Response $response) {
     static $satisfied;
-
     if (!isset($satisfied)) {
       $toggle = $this->config->get('status_toggle');
       $statuses = $this->config->get('status_list');
-
       if (!$toggle) {
         return FALSE;
       }
@@ -224,7 +251,6 @@ EOS;
     }
     return $satisfied;
   }
-
   /**
    * Determines whether or not the tag should be included on a page based on
    * the path settings.
@@ -236,13 +262,11 @@ EOS;
    *   True if the tag should be included for the current request based on path
    *   settings.
    */
-  private function pathCheck(Request $request) {
+  protected function pathCheck(Request $request) {
     static $satisfied;
-
     if (!isset($satisfied)) {
       $toggle = $this->config->get('path_toggle');
       $pages = Unicode::strtolower($this->config->get('path_list'));
-
       if (empty($pages)) {
         return ($toggle == GOOGLE_TAG_DEFAULT_INCLUDE) ? TRUE : FALSE;
       }
@@ -254,10 +278,8 @@ EOS;
         $satisfied = ($toggle == GOOGLE_TAG_DEFAULT_INCLUDE) ? !$satisfied : $satisfied;
       }
     }
-
     return $satisfied;
   }
-
   /**
    * Determines whether or not the tag should be included on a page based on
    * user roles.
@@ -265,13 +287,11 @@ EOS;
    * @return bool
    *   True is the check is enabled and the user roles match the settings.
    */
-  private function roleCheck() {
+  protected function roleCheck() {
     static $satisfied;
-
     if (!isset($satisfied)) {
       $toggle = $this->config->get('role_toggle');
       $roles = $this->config->get('role_list');
-
       if (empty($roles)) {
         return ($toggle == GOOGLE_TAG_DEFAULT_INCLUDE) ? TRUE : FALSE;
       }
@@ -284,7 +304,6 @@ EOS;
     }
     return $satisfied;
   }
-
   /**
    * {@inheritdoc}
    */
